8aa657452d575b3305ffb5aa57ad17602ed1c799
anaclara-gf <anaclaragf@outlook.com>
2021-01-31 16:40:04 -0300

    test

---
+++ src/main.js
@@ -0,0 +1,20 @@
+let myHeaders = new Headers({
+    'Accept': '*/*',
+    'Accept-Language': '*',
+    'Content-Language': '*',
+    'Content-Type': 'text/plain'
+});
+
+let myInit = { 
+    method: 'GET',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default' 
+};
+
+fetch('https://www.wikiart.org/en/api/2/PaintingSearch?term=Abaporu', myInit)
+    .then(response => {return response.json()})
+    .then((result) => {
+        console.log(result)
+    })
+
